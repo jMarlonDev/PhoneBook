2ede6cdd6a4c4330b02574b9477455205bad700e
Jhon Marlon Florez <jmarloncin07@gmail.com>
2025-01-22 11:05:34 -0500

    Refactorización del código:se mejoró la funcion para obtener los datos y se mapearon los documentos con el valor de su id con el valor del _id, se mejoró la funcion para eliminar un contacto y que este cambio se vea inmediatamente enla interfaz,

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -47,23 +47,38 @@ const App = () => {
    * los cambios en el arreglo si se elimino , creo o actulizo algo en la interfaz para que 
    * así el servidor pueda mostrar correctamente los datos
    */
+
   const [persons, setPersons] = useState([]);
+
 
   /*
    * Se utiliza el hook useEffect para realizar la llamada a la funcion getAllPersons del archivo personServices que es 
    * donde se definieron las funciones y en este caso el efecto lo que va a hacer es hacer la solicitud para obtener 
    * todos los datos del servidor y los va a guardar en un array este array sera el nuevo valor que va a tener el estado de 
    * setPerson mostrando los datos en la interfaz
+   * lo primero es que cuando se envíe la respuesta del servidor que trae los datos de mongoDB 
+   * debemos mapear esos datos que que el valor del _id que es lo que trabaja mongo sea el valor 
+   * del id que ya tenemos definido en el arreglo y lo mostramos actualizando la función de 
+   * setPersons
    */
 
-  useEffect(() => {
-    personServices.getAllPersons()
-      .then((arrayPerson) => {
-        setPersons(arrayPerson);
-      })
-  }, [])
+   useEffect(() => {
+    const fetchPersons = async () => {
+      try {
+        const arrayPerson = await personServices.getAllPersons();
+        const mappedPersons = arrayPerson.map((person) => ({
+          ...person,
+          id: person._id
+        }))
 
+        setPersons(mappedPersons);
 
+      } catch (err) {
+        console.log("Ha ocurrido un error al obtener los contactos");
+      }
+    };
+    fetchPersons();
+  }, []);
   /*
   Recordar que como el useEffect este hook necesita unas dependencias para poder ejecutarse y saber cuando debe hacerlo
   entonces dentro de las dependencias le indicamos que quiero que se llame o se pida todos los datos del servidor para mostrarlo 
@@ -74,32 +89,58 @@ const App = () => {
 
 
   /*
-   * La funcion para eliminar una persona o elemento en la interfaz:
-   * lo que hace esta funcion es primero buscar en el array actual si se encuentra el id que esta funcion recibe 
-   * como parametro de ser el caso que el id se encuentre entonces se procede a preguntar
-   * usando el evento general window.confirm() con esto preguntamos al usuario en un cuadro de dialogo
-   * si realmente quiere eliminar el elemento si la persona le da en marcar OK entonces el elemento se elimina 
-   * y ya no se muestra en la interfaz e igualmente este elemento también se elimina del servidor simulado
+   * la funcion para eliminar un contacto: 
+   * esta función lo que hace es primero preguntar si realmente queremos eliminar este contacto 
+   * si el usuario confirma entonces vamos a obtner el valor del id de ese elemento 
+   * pero para hacer est primero debemos obtner el valor de ese nuevo id ya que se esta trabajando con 
+   * mongoDB entonces mongo almacena los datos con el valor de _id y nosotros si viene el arreglo de objetos
+   * con ese valor vamos a darle ese valor a la variable de id
+   * y vamos a llamar la funcion de deletePerson  para eliminar ese arreglo 
+   * y luego para poder ver ese cambio directamente en al interfaz 
+   * vamos a recorrer el arreglo de personas que ya obtuvimos 
+   * y vamos a mostrar solo los elementos qe sean diferentes del id que yo ya elimine
    */
-
-  const handleDelete = async (id) => {
+  const handleDelete = async (person) => {
     if (window.confirm("¿Estas seguro de que quieres eliminar este contacto?")) {
       try {
-        const updatePersons = await personServices.deletePerson(id);
-        setPersons(updatePersons);
+        const id = person.id ?? person._id;
+        await personServices.deletePerson(id);
+        setPersons(prevPersons => prevPersons.filter(personx => (personx.id ?? personx._id) !== id));
+
       } catch (error) {
-        console.log("ha ocurrido un error al intentar eliminar el elemento", error);
+        console.log("Ha ocurrido un error al eliminar el contacto");
       }
     }
   };
 
+  const handlePerson = async (person) => {
+    try {
+      let response = null;
+      if (person.id ?? person._id) {
+        const id = person.id ?? person._id;
+        response = await personServices.updatePhonePerson(id, person);
+      } else {
+        response = await personServices.createPerson(person);
+      }
+
+      setPersons((prevPersons) => {
+        const id = response.id ?? response._id;
+        return person.id || person._id ? prevPersons.map(prevPerson => (prevPerson.id ?? prevPerson._id) === id ? { ...response, id: id } : prevPerson) : [...prevPersons, { ...response, id: id }]
+      })
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
   return (
     <>
       <h2>PhoneBook</h2>
-      <Filter arrayPerson={persons} />
+      <Filter arrayPersons={persons} />
       <h3>Add A New Contact</h3>
-      <PersonForm arrayPerson={persons} setPersons={setPersons} />
-      <Persons arrayPersons={persons} handleDelete={handleDelete} />
+      <PersonForm arrayPerson={persons} setPersons={handlePerson} />
+      {persons && <Persons arrayPersons={persons} handleDelete={handleDelete} />}
     </>
   )
 }

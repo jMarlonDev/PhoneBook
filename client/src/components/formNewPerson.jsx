a488011642b0b935dc6ad2d716fc4bb9147d2e78
Jhon Marlon Florez <jmarloncin07@gmail.com>
2025-01-22 11:18:05 -0500

    Se mejoró al funcion para crear un nuevo contacto en el componente del formulario

--- client/src/components/formNewPerson.jsx
+++ client/src/components/formNewPerson.jsx
@@ -18,100 +18,52 @@
  * con Axios.
 */
 import { useState } from "react";
-import personServices from "../services/persons.js";
 import "../App.css"
 
-/* El componente PersonForm recibe como parametros arrayPerson ( que es el arreglo que contiene 
- * todos los elementos o contactos ) , setPersons() que es la funcion encargada de actualizar los 
- * valores del arreglo cada vez que se agrega , elimina , actualiza o obtiene los elementos 
- * esta funcion se encargara de mantener los datos bien actualizados para poder mostrar los cambios 
-*/
 const PersonForm = ({ arrayPerson, setPersons }) => {
-  /* Variables de estado y funciones que se van a encargar de actualizar estas variables */
-  const [newName, setNewName] = useState(""); // se actualiza el valor de la variable cada vez que yo ingreso un nuevo nombre
-  const [newNumber, setNewNumber] = useState("");// se actualiza el valor de la variable cada vez que yo ingreso un nuevo telefono
+  // variables de estado para cambiar y actualizar los valores 
+  // de el nombre y el telefono ingresado en el input
+  const [newName, setNewName] = useState(""); 
+  const [newNumber, setNewNumber] = useState("");
 
-  /* Función handleSubmitForm Es al encargada de enviar los datos que se ingresaron en el formulario con los campos 
-   * de Name y Phone,
-   * Como funciona: 
+  /* 
+   * La funcion handleSubmitForm lo que hace evitar el comportamiento por defecto de recargar la pagina
+   * y creamos un nuevo objeto newPerson y le vamos a dar los valores que tenga actualemente en las 
+   * variables de newName y newPerson para que sean las propiedades del nuevo objeto,
+   * luego verificamos si el nombre que yo estoy ingresando para crear un nuevo contacto 
+   * si ese nombre ya existe entonces le indicamos con una ventana emergente 
+   * para decir que el contacto ya existe pero si queremos podemos actualizar el numero de telefono 
+   * de aquel contacto y el cambio se hara inmediatamente en al interfaz
+   * y actualizamos el valor del numero de telefono del usuario y lo envíamos en la funcion de 
+   * actualización del arreglo de contacts para mostrar los cambios en la interfaz
    *
-   * 1. primero evita el comportamiento por defecto que tienen los formulario al enviar los datos, lo que hace 
-   * es recargar de nuevo la pagina pero en este caso la pagina ya no se va a recargar
-   *
-   * 2. crea un nuevo objeto el cual tiene las propiedades que de name , phone y su valor 
-   * va a depender del que tenga el newName y newNumber cuando en la interfaz se ingrese el nombre y el telefono 
-   * ese nuevo objeto el que vamos a crear va a contener ese valores ingresados
-   *
-   * 3. Hace una verificación con el atributo name del objeto y la propiedad name de los elementos 
-   * que estan guardados en el arreglo para comprobar si el elemento que yo esoty intentanto crear 
-   * ya existe dentro del arreglo de elementos y lo guarda en la variable existingPerson
-   * 
-   * Dado el caso que el usuario exista dentro de mi arreglo de elementos 
-   * pues entonces vamos a mostrar un mensaje emergente con el metodo confirm() 
-   * Message: el usuario ya existe pero deseas cambiar o reemplazar el numero de telefono 
-   * si la respuesta es si entonces devolvemos en la interfaz el nuevo elemento con el mismo
-   * nombre pero con el teelfono actualizado
-   * Si la respuesta es NO entonces devolvemos un mensaje de error
-   * 4. Si ya se hizo toda la comprobación y los datos que se estan ingresando no corresponde
-   * con ningún elemento dentro del arreglo de entonces llamamos a la funcion para crear un 
-   * nuevo elemento en la interfaz y se meustra
-   */
-
+   * Pero si despues de verificar si el nombre ingresado no existe entonces se trata de una creación 
+   * de un nuevo contacto entonces envíamos a la función para actualizar 
+   * el arreglo de contactos y le envíamos el nuevo contacto que queremos visualizar en la interfaz
+   * */
   const handleSubmitForm = async (event) => {
-
     event.preventDefault();
     const newPerson = {
       name: newName,
       phone: newNumber
-    }
-
+    };
     const existingPerson = arrayPerson.find((person) => person.name === newPerson.name);
-
     if (existingPerson) {
-      const confirmation = window.confirm(
-        `The contact "${newPerson.name}" already exists. Do you want to replace the phone number?`
-      );
-
+      const confirmation = window.confirm(`The contact "${newPerson.name}" already exists. Do you want to replace the phone number?`);
       if (confirmation) {
-        try {
-          const updatedPerson = { ...existingPerson, phone: newNumber };
-          const responseUpdate = await personServices.updatePhonePerson(existingPerson.id, updatedPerson);
-
-          setPersons((prevPersons) =>
-            prevPersons.map((person) =>
-              person.id === responseUpdate.id ? responseUpdate : person
-            )
-          );
-
-        } catch (error) {
-          console.log("ha ocurrido un error", error)
-        }
-
+        setPersons({ ...existingPerson, phone: newPerson.phone });
       }
+
     } else {
-      try {
-        const createdPerson = await personServices.createPerson(newPerson);
-        setPersons((prevPersons) => [...prevPersons, createdPerson]);
-
-      } catch (error) {
-        console.log(`Failed to add "${newPerson.name}". Please try again.`);
-      }
+      setPersons(newPerson);
     }
-
     setNewName("");
     setNewNumber("");
-
-    setTimeout(() => setNotification(null), 3000);
   };
 
-  /* Controladores de eventos para los campos de ( Input ) del formulario 
-   * 1. handleInputValue : se encarga de obtener el valor ingresado en el campo del nombre (Name)
-   * y lo agrega a la variable de newName por medio de la función setNewName
-   *
-   * 2. handleInputNumber: se encarga de obtener el valor ingresado en el campo de telefono ( Phone )
-   * y lo agrega a la variabe de newNumber por medio de la función setNewNumber
-   */
-
+ /* Contraladores de eventos para obtener el valor ingresado en los campos de input 
+  * name y phone en la interfaz y también se encarga de actualizar las variables de newName y newPerson 
+  * para que cada vez que se ingrese un nuevo nombre y telefono en los campos estas variables van a tomar ese valor*/
   const handleInputValue = (event) => {
     let value = event.target.value;
     setNewName(value);
@@ -122,15 +74,14 @@ const PersonForm = ({ arrayPerson, setPersons }) => {
     setNewNumber(value);
   }
 
-
   return (
     <div className="containerForm">
       <form onSubmit={handleSubmitForm} className="form">
         <div className="inputName">
-          Name: <input value={newName} onChange={handleInputValue} className="inputForm" />
+          Name: <input value={newName} onChange={handleInputValue} className="inputForm" required />
         </div>
         <div className="inputPhone">
-          Phone: <input value={newNumber} onChange={handleInputNumber}  className="inputForm"/>
+          Phone: <input value={newNumber} onChange={handleInputNumber} className="inputForm" required />
         </div>
         <div>
           <button type='submit' className="submitBtn">Add</button>
@@ -138,6 +89,6 @@ const PersonForm = ({ arrayPerson, setPersons }) => {
       </form>
     </div>
   );
-}
+};
 
 export default PersonForm;

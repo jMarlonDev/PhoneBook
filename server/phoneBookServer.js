fb5242498493e9fc3a0d85c1427cf63e5a1d84a9
Jhon Marlon Florez <jmarloncin07@gmail.com>
2025-01-22 12:10:01 -0500

    Actualizando la url del servidor para que se conecte con el cliente el puerto 5173

--- server/phoneBookServer.js
+++ server/phoneBookServer.js
@@ -24,7 +24,7 @@ const path = require('path'); // Importa el módulo path para manejar archivos d
 app.use(express.json())
 
 app.use(cors({
-  origin: "http://localhost:5173/", 
+  origin: "*", // Permitir cualquier dominio
 }));
 
 mongoose.connect(urlMongo)
